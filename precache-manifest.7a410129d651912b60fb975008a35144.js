3815b6ac090ff4cc4a4c7ff3e19bb6e53b6bc67e
Nikita Remeslov <creatorbon@gmail.com>
2019-04-09 15:42:30 +0300

    Updates

---
+++ precache-manifest.7a410129d651912b60fb975008a35144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a3b572adee98ef955d",
+    "url": "/react-cv/static/js/runtime~main.d42d17c0.js"
+  },
+  {
+    "revision": "2849c829c517a1305a1c",
+    "url": "/react-cv/static/js/main.f588e3b3.chunk.js"
+  },
+  {
+    "revision": "92bc6412fef04c74336a",
+    "url": "/react-cv/static/js/2.6e9591ff.chunk.js"
+  },
+  {
+    "revision": "2849c829c517a1305a1c",
+    "url": "/react-cv/static/css/main.4a4e841f.chunk.css"
+  },
+  {
+    "revision": "92bc6412fef04c74336a",
+    "url": "/react-cv/static/css/2.d9553b0d.chunk.css"
+  },
+  {
+    "revision": "43e5c6f2907c230b8cf79084cbcc6057",
+    "url": "/react-cv/index.html"
+  }
+];
