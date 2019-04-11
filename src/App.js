93fb11dc48b9f1f0e6b4c3b10634918fb0a2a517
Nikita Remeslov <creatorbon@gmail.com>
2019-04-11 14:51:03 +0300

    little fix

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="header">
-          <nav className="navbar navbar-expand-lg navbar-dark position-fixed">
+          <nav className="navbar navbar-expand-lg navbar-dark">
             <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
